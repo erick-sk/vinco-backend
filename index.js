07521f657b6756cc2963bd64dd5a23ed54699d28
Erick Saavedra <esvdrah@gmail.com>
2021-11-19 10:43:44 -0500

    Add dotenv dependece and the connection to the database established

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // create server
 const app = express();
+
+// MongoDB connection
+const database = process.env.MONGODB_URI;
+mongoose
+  .connect(database, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected!"))
+  .catch((error) => console.log(error));
 
 // port
 const PORT = process.env.PORT || 4000;

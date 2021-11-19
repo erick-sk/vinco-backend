38ea2a22a4135f7b0e2e75d987bf21e2c225f336
Erick Saavedra <esvdrah@gmail.com>
2021-11-19 12:49:46 -0500

    Create a record in the database

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,6 +1,17 @@
+const Patient = require('../models/Patient');
+
 // when a new customer is created
-exports.newCustomer = (req, res, next) => {
-  // TODO : Insert in the database
-  console.log(req.body);
-  res.json({ message: 'Customer was successfully added' });
+exports.newCustomer = async (req, res, next) => {
+  // create object of patient with data from req.body
+  const patient = new Patient(req.body);
+
+  try {
+    await patient.save();
+    res.json({
+      message: 'Customer was successfully added',
+    });
+  } catch (error) {
+    console.log(error);
+    next();
+  }
 };
